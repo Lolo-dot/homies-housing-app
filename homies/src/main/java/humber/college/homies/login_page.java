38975feec61db79a853025ca2028f698d83be3c7
ShermalM <n01345430@humbermail.ca>
2020-10-13 21:54:39 -0400

    Created layout for login page

--- homies/src/main/java/humber/college/homies/login_page.java
+++ homies/src/main/java/humber/college/homies/login_page.java
@@ -1,4 +1,16 @@
 package humber.college.homies;
 
-public class login_page {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+            setContentView(R.layout.login_layout);
+
+    }
+
 }
