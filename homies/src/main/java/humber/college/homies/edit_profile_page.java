6cce1d379a06c950a63610121d32e1c77f4d4b6c
igobylolo <n01325054@humbermail.ca>
2020-10-14 01:59:34 -0400

    made the edit profile page

--- homies/src/main/java/humber/college/homies/edit_profile_page.java
+++ homies/src/main/java/humber/college/homies/edit_profile_page.java
@@ -9,6 +9,6 @@ public class edit_profile_page extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_layout);
+        setContentView(R.layout.edit_profile_layout);
     }
 }
