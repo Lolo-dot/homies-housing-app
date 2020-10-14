4068ad5225395be89b6524eab41e81450a6b3d23
gurarsh707 <71414880+gurarsh707@users.noreply.github.com>
2020-10-14 01:38:54 -0400

    Navigation Bar working fully with functionality on Search and Profile Page

--- homies/src/main/java/humber/college/homies/MainActivity.java
+++ homies/src/main/java/humber/college/homies/MainActivity.java
@@ -1,8 +1,14 @@
 package humber.college.homies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_layout);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent = null;
+                switch (item.getItemId()) {
+                    case R.id.s:
+                        intent = new Intent(getBaseContext(), MainActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.m:
+                        break;
+                    case R.id.p:
+                        intent = new Intent(getBaseContext(), profile_page.class);
+                        startActivity(intent);
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
     }
+
+
 }
--- homies/src/main/java/humber/college/homies/bookmark_page.java
+++ homies/src/main/java/humber/college/homies/bookmark_page.java
@@ -1,4 +1,14 @@
 package humber.college.homies;
 
-public class bookmark_page {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bookmark_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bookmark_layout);
+    }
 }
