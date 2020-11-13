eb85e73a225fdcfe569757a7b64cc81aabca2885
n01335381 <71400096+n01335381@users.noreply.github.com>
2020-11-12 23:14:36 -0500

    attemtped firebase but ran into issues. need to redo connection, but cannot instantly delete. will attempt to shift project later???

--- homies/src/main/java/humber/college/homies/message_page.java
+++ homies/src/main/java/humber/college/homies/message_page.java
@@ -6,16 +6,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class message_page extends AppCompatActivity {
+    public Button writeToDB;
+    public EditText edBox;
+    public TextView textBox;
+
+    //private FirebaseDatabase database;
+    //private DatabaseReference myRefTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message_layout);
+
+        writeToDB = (Button) findViewById(R.id.btWriteToDatabase);
+        writeToDB.setOnClickListener(onClickWrite);
+        textBox = (TextView)findViewById(R.id.tvMessages);
+
+        edBox=(EditText)findViewById(R.id.edMessages);
+
+        //database = FirebaseDatabase.getInstance();
+        //myRefTest = database.getReference("TestMessage");
+
+        //FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //DatabaseReference myRef = database.getReference("message");
+
+       // myRefTest.setValue("Hello, World!");
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_bar);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -47,5 +72,37 @@ public class message_page extends AppCompatActivity {
         });
     }
 
+    public void testFireBaseWrite(String value){
+       // FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //DatabaseReference myRef = database.getReference("message");
+
+        //myRef.setValue("Hello, World!");
+        //myRef.setValue(value);
+        //Toast.makeText(getApplicationContext(),"writing "+value+" to FireBase!",Toast.LENGTH_SHORT).show();
+        //myRef.addValueEventListener(new ValueEventListener() {
+            //@Override
+            //public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //String value = dataSnapshot.getValue(String.class);
+                //textBox.setText(value);
+            //}
+
+            //@Override
+            //public void onCancelled(DatabaseError error) {
+             //   Toast.makeText(getApplicationContext(),"failed to get value",Toast.LENGTH_SHORT).show();
+            //}
+        //});
+    }
+
+
+    public View.OnClickListener onClickWrite= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String data= edBox.getText().toString();
+            //Toast.makeText(getApplicationContext(),"writing "+data+" to FireBase!",Toast.LENGTH_SHORT).show();
+            testFireBaseWrite(data);
+        }
+    };
 
 }
