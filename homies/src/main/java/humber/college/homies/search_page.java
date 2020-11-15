d665f3fb38095496e768e31939b9548ea2c9c199
n01335381 <71400096+n01335381@users.noreply.github.com>
2020-11-15 18:01:39 -0500

    added some recycler view testing for rehauling search page. added draaables, dimens, styles, several java files and layouts.

--- homies/src/main/java/humber/college/homies/search_page.java
+++ homies/src/main/java/humber/college/homies/search_page.java
@@ -2,20 +2,72 @@ package humber.college.homies;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 public class search_page extends AppCompatActivity {
+
+    SearchView sv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_layout);
+        setContentView(R.layout.testsearchlayout);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        sv= (SearchView) findViewById(R.id.mSearch);
+        RecyclerView rv= (RecyclerView) findViewById(R.id.myRecycler);
+
+        //SET ITS PROPETRIES
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setItemAnimator(new DefaultItemAnimator());
+
+        //ADAPTER
+        final MyAdapter adapter=new MyAdapter(this,getPlayers());
+        rv.setAdapter(adapter);
+
+        //SEARCH
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                //FILTER AS YOU TYPE
+                adapter.getFilter().filter(query);
+                return false;
+            }
+        });
+
+
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_bar);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -45,6 +97,49 @@ public class search_page extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    private ArrayList<Player> getPlayers()
+    {
+        ArrayList<Player> players=new ArrayList<>();
+        Player p=new Player();
+        p.setName("Ander Herera");
+        p.setPos("Midfielder");
+        p.setImg(R.drawable.herera);
+        players.add(p);
+
+        p=new Player();
+        p.setName("David De Geaa");
+        p.setPos("Goalkeeper");
+        p.setImg(R.drawable.degea);
+        players.add(p);
+
+        p=new Player();
+        p.setName("Michael Carrick");
+        p.setPos("Midfielder");
+        p.setImg(R.drawable.carrick);
+        players.add(p);
+
+        p=new Player();
+        p.setName("Juan Mata");
+        p.setPos("Playmaker");
+        p.setImg(R.drawable.mata);
+        players.add(p);
+
+        p=new Player();
+        p.setName("Diego Costa");
+        p.setPos("Striker");
+        p.setImg(R.drawable.costa);
+        players.add(p);
+
+        p=new Player();
+        p.setName("Oscar");
+        p.setPos("Playmaker");
+        p.setImg(R.drawable.oscar);
+        players.add(p);
+
+
+        return players;
     }
 
 
