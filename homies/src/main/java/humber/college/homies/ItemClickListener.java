ff08a1af5856cc0ecb3e211c99db7c5a2d185585
n01335381 <71400096+n01335381@users.noreply.github.com>
2020-11-16 18:43:08 -0500

    added writing from searched to databse, and reading from bookmarked houses from data base ( aka saving houses). need to make message and bookmark buttons seperate, as well as remove buttons.

--- homies/src/main/java/humber/college/homies/ItemClickListener.java
+++ homies/src/main/java/humber/college/homies/ItemClickListener.java
@@ -2,9 +2,6 @@ package humber.college.homies;
 
 import android.view.View;
 
-/**
- * Created by Hp on 3/17/2016.
- */
 public interface ItemClickListener {
 
     void onItemClick(View v, int pos);
