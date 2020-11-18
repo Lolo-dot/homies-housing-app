a4287cc50300edf532d0ee040b1a3018333d5a42
ShermalM <72827005+ShermalM@users.noreply.github.com>
2020-11-18 13:43:13 -0500

    Added backpress to login page

--- homies/src/main/java/humber/college/homies/login_page.java
+++ homies/src/main/java/humber/college/homies/login_page.java
@@ -1,5 +1,6 @@
 package humber.college.homies;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
@@ -86,6 +88,22 @@ public class login_page extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+        builder.setMessage(R.string.Exit_Confirmation);
+        builder.setCancelable(true);
+
+        builder.setPositiveButton(R.string.Yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     public void Go_To_Signup(View view){
