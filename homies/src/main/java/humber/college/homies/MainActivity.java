56d2117194e4d1ebac9369e22c6cfdf0484b9e26
ShermalM <72827005+ShermalM@users.noreply.github.com>
2020-12-08 22:49:27 -0500

    Fixed Dark Mode Bugs

--- homies/src/main/java/humber/college/homies/MainActivity.java
+++ homies/src/main/java/humber/college/homies/MainActivity.java
@@ -3,12 +3,14 @@ package humber.college.homies;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -34,6 +36,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
     public MainActivity(){
         super(R.layout.searchlayout);
     }
@@ -48,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchlayout);
-
         final Fragment searchFrag = new Search_page();
         final Fragment bookmarkFrag = new Bookmark_page();
         final Fragment profileFrag = new Profile_page();
