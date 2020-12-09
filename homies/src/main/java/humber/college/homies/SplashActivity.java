9588bce292b319e7b3ff50fce662326d1b761b56
gurarsh707 <71414880+gurarsh707@users.noreply.github.com>
2020-12-09 16:38:49 -0500

    Added Splash Screen

---
+++ homies/src/main/java/humber/college/homies/SplashActivity.java
@@ -0,0 +1,29 @@
+package humber.college.homies;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+        getSupportActionBar().hide();
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), Login_page.class);
+                startActivity(intent);
+            }
+        }, 3000);
+    }
+
+
+}
