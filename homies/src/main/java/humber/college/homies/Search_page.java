d0a96622b81f7381b8b400e8df31160ff54027b1
n01335381 <71400096+n01335381@users.noreply.github.com>
2020-12-11 21:35:55 -0500

    cleaned up search page

--- homies/src/main/java/humber/college/homies/Search_page.java
+++ homies/src/main/java/humber/college/homies/Search_page.java
@@ -2,23 +2,14 @@ package humber.college.homies;
 //Team Name: Homies
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -26,10 +17,6 @@ import android.widget.ArrayAdapter;
 import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-import com.facebook.AccessToken;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -46,8 +33,6 @@ public class Search_page extends Fragment {
     SearchView sv;
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     public ArrayList<House> housesList = new ArrayList<>();
-    public ArrayList<House> userHouses = new ArrayList<>();
-    public ArrayList<House> userBookmarkedHouses = new ArrayList<>();
     RecyclerView rv;
     MyAdapter adapter;
     String username;
@@ -62,24 +47,10 @@ public class Search_page extends Fragment {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.remove(getString(R.string.number_key_pref)).apply(); //removing old shared pref of phone number/email
-
         username = sharedPreferences.getString("usernameStorage","failed");
 
-
-        //floating action circle/bar. probs not needed
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, getString(R.string.snack_bar_value), Snackbar.LENGTH_LONG)
-                        .setAction(getString(R.string.action_snack_bar), null).show();
-            }
-
-        });*/
-
+        //sort spinner adpater and functionality
         final Spinner spinner = (Spinner) view.findViewById(R.id.searchSortsSpinner);
-
 
         ArrayAdapter<CharSequence> adapterSpinner = ArrayAdapter.createFromResource(view.getContext(), R.array.sortTypes,
                 android.R.layout.simple_spinner_item);
@@ -91,48 +62,37 @@ public class Search_page extends Fragment {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id)
             {
-                //Toast.makeText(getContext(),"price low to hhhhigh selected",Toast.LENGTH_SHORT).show();
                 if(sv.getQuery().toString()==null||sv.getQuery().toString().equals("")) {
                     switch (parentView.getItemAtPosition(position).toString()) {
                         case "Sort":
-                            //Toast.makeText(getContext(),"Sort",Toast.LENGTH_SHORT).show();
-
                             break;
                         case "Price:Low to High":
-                            //Toast.makeText(getContext(),"price low to high selected",Toast.LENGTH_SHORT).show();
                             Collections.sort(housesList, House.PriceLowTooHigh);
                             adapter.notifyDataSetChanged();
                             break;
                         case "Price:High to Low":
-                            //Toast.makeText(getContext(),"Price:High to Low",Toast.LENGTH_SHORT).show();
                             Collections.sort(housesList, House.PriceHighToLow);
                             adapter.notifyDataSetChanged();
                             break;
                         case "Alphabetical:A-Z":
-                            //Toast.makeText(getContext(),"Alphabetical:A-Z",Toast.LENGTH_SHORT).show();
                             Collections.sort(housesList, House.NameAToZ);
                             adapter.notifyDataSetChanged();
                             break;
                         case "Alphabetical:Z-A":
-                            //Toast.makeText(getContext(),"Alphabetical:Z-A",Toast.LENGTH_SHORT).show();
                             Collections.sort(housesList, House.NameZToA);
                             adapter.notifyDataSetChanged();
                             break;
                         default:
-                            //Toast.makeText(getContext(),"default break",Toast.LENGTH_SHORT).show();
                             break;
                     }//end of switch
                 } else{
                     spinner.setSelection(0);
-                    //Toast.makeText(getContext(),"Search bar must be empty to change sort",Toast.LENGTH_SHORT).show();
-                    Snackbar snackbar = Snackbar.make(getActivity().findViewById(R.id.constraintLayout), "Search bar must be empty to change sort", Snackbar.LENGTH_LONG);snackbar.show();
+                    Snackbar snackbar = Snackbar.make(getActivity().findViewById(R.id.constraintLayout), getString(R.string.emptysearchbar), Snackbar.LENGTH_LONG);snackbar.show();
                 }
             }//end of onitme selected
 
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
-                // your code here
-                Toast.makeText(getContext(),"price low to high sssssselected",Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -147,10 +107,8 @@ public class Search_page extends Fragment {
         //setting adapter to recylcer view, of custome type  MyAdapter
         rv.setAdapter(adapter);
 
-        //getting master "Houses" list to assign to "userHouses" array list, (need checks vs bookmarked houses)
-        //final DatabaseReference myRef = database.getReference("USER/"+username);
+        //getting master "Houses" list to assign to "userHouses" array list
         final DatabaseReference refUserName = FirebaseDatabase.getInstance().getReference().child("USER").child(username);//reference to specificshared pref username
-        //refUserName.child("userHouses").removeValue(); //removing current houses list
         DatabaseReference refHouses = database.getReference(getString(R.string.database_ref_search_gae)); //reference to master "Houses"
 
         ChildEventListener masterList = new ChildEventListener() {
@@ -176,7 +134,6 @@ public class Search_page extends Fragment {
             @Override
             public void onChildRemoved(@NonNull DataSnapshot snapshot) {
                 House p = snapshot.getValue(House.class);
-                //Toast.makeText(getActivity(), refUserName.child("userHouses").child(p.getName())+" was removed",Toast.LENGTH_SHORT).show();
                 refUserName.child("userHouses").child(p.getName()).removeValue();
                 refUserName.child("userBookmarkedHouses").child(p.getName()).removeValue();
             }
